90ca59683e2567bd263a59e52d59d8abda3d9f05
Dameli <dameli.ryspay@gmail.com>
2021-10-07 20:41:38 +0600

    add get all posts db query

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -65,4 +65,23 @@ router.get("/:id", async (req, res) => {
   }
 });
 
+// Get all posts
+router.get("/", async (req, res) => {
+  const username = req.query.user;
+  const category = req.query.category;
+  try {
+    let posts;
+    if (username) {
+      posts = await Post.find({ username: username });
+    } else if (category) {
+      posts = await Post.find({ categories: { $in: [category] } });
+    } else {
+      posts = await Post.find();
+    }
+    res.status(200).json(posts);
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
+});
+
 module.exports = router;
